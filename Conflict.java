25e201595f72004254cae1d3785c9305c883a7ce
manabu fujikura <m.fujikura.1811@morijyobi.ac.jp>
2019-06-04 12:11:26 +0900

    　[add]Conflict.javaの作成d

--- Conflict.java
+++ Conflict.java
@@ -1,4 +1,23 @@
-publi static void main(string[] args){
-	System.out.println("Heoll world");
+public static void main(string[] args){
+  System.out.println("Hellowrold");
+}
+
+import java.util.Random;
+
+public class RandomTest {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        for(int i=0 ; i<10 ; i++) {
+            int val = (int)(rnd.nextDouble() * 100.0);
+            System.out.println((i+1) + "つ目:" + val);
+        }
+    }
+
+
+        
+
+
+
+	
 
 }
